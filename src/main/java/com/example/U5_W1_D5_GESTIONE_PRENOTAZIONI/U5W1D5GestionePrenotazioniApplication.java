dd69e36066c4e2056f570e2cd84a95425c111331
Antonio <tony.formisano.2000@gmail.com>
2026-07-03 10:31:21 +0200

    BASE

---
+++ src/main/java/com/example/U5_W1_D5_GESTIONE_PRENOTAZIONI/U5W1D5GestionePrenotazioniApplication.java
@@ -0,0 +1,13 @@
+package com.example.U5_W1_D5_GESTIONE_PRENOTAZIONI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5W1D5GestionePrenotazioniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5W1D5GestionePrenotazioniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/U5_W1_D5_GESTIONE_PRENOTAZIONI/U5W1D5GestionePrenotazioniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.U5_W1_D5_GESTIONE_PRENOTAZIONI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5W1D5GestionePrenotazioniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
